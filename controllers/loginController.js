5d99a18b57bd3c5dc28dcf72806f5b66a72548bf
Hyomk <qet557@gmail.com>
2022-08-18 15:59:05 +0900

    fix : 로그인 라우팅 수정

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,82 +1,85 @@
 const bodyParser = require("body-parser");
-const nunjucks = require('nunjucks');
-const axios = require('axios');
-const qs = require('qs');
-const session = require('express-session');
-const db = require("../config/db")
-
-const kakao={
-    clientID : '187dfbd8517a2fdb721af0f630552827',
-    clientSecret : '08NwRRAOnO5kKs8pCdYsU953VMna3A3T',
-    redirectUri: 'http://localhost:3000/auth/kakao/callback'
-}
+const nunjucks = require("nunjucks");
+const axios = require("axios");
+const qs = require("qs");
+const session = require("express-session");
+const db = require("../config/db");
 
 module.exports = {
     getKakaoLogin: (req, res) => {
-        const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile,account_email`;
+        const kakao = {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            redirectUri: process.env.REDIRECT_URI,
+        };
+        const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile_nickname,profile_image,account_email`;
         res.redirect(kakaoAuthURL);
     },
     loginProcess: async (req, res) => {
+        const kakao = {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            redirectUri: process.env.REDIRECT_URI,
+        };
         let token;
         try {
             token = await axios({
-                method: 'POST',
-                url: 'https://kauth.kakao.com/oauth/token',
+                method: "POST",
+                url: "https://kauth.kakao.com/oauth/token",
                 headers: {
-                    'content-type': 'application/x-www-form-urlencoded'
+                    "content-type": "application/x-www-form-urlencoded",
                 },
                 data: qs.stringify({
-                    grant_type: 'authorization_code',
+                    grant_type: "authorization_code",
                     client_id: kakao.clientID,
                     client_secret: kakao.clientSecret,
                     redirectUri: kakao.redirectUri,
                     code: req.query.code,
-                })
-            })
+                }),
+            });
         } catch (err) {
-            res.json(err.data)
+            res.json(err.data);
         }
-    
+
         let user;
-        try{
+        try {
             user = await axios({
-                method:"GET",
-                url:'https://kapi.kakao.com/v2/user/me',
-                headers:{
-                    Authorization:`Bearer ${token.data.access_token}`
-                }
-            })
-        }catch(err){
+                method: "GET",
+                url: "https://kapi.kakao.com/v2/user/me",
+                headers: {
+                    Authorization: `Bearer ${token.data.access_token}`,
+                },
+            });
+        } catch (err) {
             res.json(err.data);
         }
         console.log(user);
-    
+
         var id = user.data.id;
-        var user_name = user.data.prop.nickname;
-    
-    
+        var user_name = user.data.properties.profile_nickname;
+        const psword = "noPasswordHere";
         const query = "INSERT INTO users(id, psword) VALUES(?, ?);";
 
-        db.query(query, [id, psword], function(err, rows, fields) {
-            if(err)
-            console.log(err);
+        db.query(query, [id, psword], function (err, rows, fields) {
+            if (err) console.log(err);
             else {
-            console.log(rows);
-            res.send('회원가입이 완료되었습니다.');
+                console.log(rows);
+                res.send("회원가입이 완료되었습니다.");
             }
-        })
+        });
 
         req.session.kakao = user.data;
 
-        res.redirect('/');
+        res.redirect("/");
     },
-    getinfo: (req,res) => {
-        let {nickname,profile_image} = req.session.kakao.properties
-        res.render('info.html',{
-            nickname,profile_image
+    getinfo: (req, res) => {
+        let { nickname, profile_image } = req.session.kakao.properties;
+        res.render("info.html", {
+            nickname,
+            profile_image,
         });
     },
-    getpage: (req,res)=>{
-        res.render('index.html');
-    }
-}
+    getpage: (req, res) => {
+        res.render("index.html");
+    },
+};
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,15 +1,12 @@
-const express = require('express');
-const { getpage } = require('../controllers/loginController');
-const loginController = require('../controllers/loginController');
-const userController = require('../controllers/userController');
+const express = require("express");
+const { getpage } = require("../controllers/loginController");
+const loginController = require("../controllers/loginController");
+const userController = require("../controllers/userController");
 const router = express.Router();
 
-router.get("/auth/kakao", loginController.getKakaoLogin);
-router.get("/auth/kakao/callback", loginController.loginProcess);
-router.get("/auth/info", loginController.getinfo);
+router.get("/kakao", loginController.getKakaoLogin);
+router.get("/kakao/callback", loginController.loginProcess);
+router.get("/info", loginController.getinfo);
 router.get("/", loginController.getpage);
-
-
-
 
 module.exports = router;
